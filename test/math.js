c3b06aafe3811230aef777f8c19678fcfeecc832
Davi Viana <davianasales@outlook.com>
2022-09-12 15:52:58 -0300

    tests(math): lint

--- test/math.js
+++ test/math.js
@@ -1,23 +1,22 @@
 // Simple math tests
 // Checks if the test suite is working
-import { describe } from 'mocha'
-import assert from 'assert'
+import { describe } from "mocha";
+import assert from "assert";
 
 function multiply(a, b) {
-    return a * b
+  return a * b;
 }
 
 function sum(a, b) {
-    return a + b
+  return a + b;
 }
 
-describe('basic math', () => {
-    it('should multiply 1 by 5', () => {
-        assert.equal(multiply(1, 10), 10)
-    })
+describe("basic math", () => {
+  it("should multiply 1 by 5", () => {
+    assert.equal(multiply(1, 10), 10);
+  });
 
-    it('should sum 400 with 200', () => {
-        assert.equal(sum(400, 200), 600)
-    })
-})
-
+  it("should sum 400 with 200", () => {
+    assert.equal(sum(400, 200), 600);
+  });
+});
